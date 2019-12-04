34077ec58dabb1dccd522d60665761b68a913be7
Ewan Burns <ewan@beezup.com>
2019-12-04 15:36:37 +0100

    React In One Minute

---
+++ 01-add-in-one-minute/scripts/pages/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// const e = React.createElement;
+// const r = React.render;
+
+let elem = document.querySelector("#like_button_container");
+
+console.log(e);
+console.log(r);
+
+r(e(LikeButton), elem);
